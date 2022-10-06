402dcebab68aa5e684689c8f57e1efac753e21d4
Raposa282 <2001499@uad.ac.uk>
2022-10-06 12:34:08 +0100

    Added calculator and converter

---
+++ Utility_Program/Utility_Program/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Utility_Program
+{
+    class Program
+    {
+        int Calculator(int a, int b, string action)
+        {
+            if (action == "add")
+            {
+                return a + b;
+            }
+            else if (action == "subtract")
+            {
+                return a - b;
+            }
+            else if (action == "multiply")
+            {
+                return a * b;
+            }
+            else if (action == "divide")
+            {
+                return a / b;
+            }
+        }
+
+        int Converter(int farenheit)
+        {
+            int celsius = farenheit - 30;
+            return celsius / 2;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
