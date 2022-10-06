6712034e96d7bd4292d599a9747d688cc898d3c8
nnja6lade <1903299@uad.ac.uk>
2022-10-06 14:39:27 +0100

    Finished the converterfunction

--- Utility_Program/Utility_Program/Program.cs
+++ Utility_Program/Utility_Program/Program.cs
@@ -22,12 +22,18 @@ namespace Utility_Program
             {
                 return a / b;
             }
+            return 0;
         }
 
         int Converter(int farenheit)
         {
-            int celsius = farenheit - 30;
-            return celsius / 2;
+            string a = "subtract";
+            string b = "divide";
+            int total = 0;
+
+            int celsius = Calculator( farenheit,30, a);
+            total = Calculator(celsius, 2, b);
+            return total;
         }
 
         static void Main(string[] args)
