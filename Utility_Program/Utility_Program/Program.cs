ccfe0f6f891872b91b8af33f61e13b29e1960bc1
Raposa282 <2001499@uad.ac.uk>
2022-10-07 09:40:00 +0100

    Finished the main function

--- Utility_Program/Utility_Program/Program.cs
+++ Utility_Program/Utility_Program/Program.cs
@@ -34,12 +34,21 @@ namespace Utility_Program
         {
             Console.WriteLine("Do you wish to use the the calculator or the Fahrenheit to Celsius converter?\n");
             Console.WriteLine("Enter 'Calculator' for the former and 'Converter' for the latter.\n");
-            string Choice = Console.ReadLine();
+            string Choice1 = Console.ReadLine();
 
             if (Choice == "Calculator")
             {
                 Console.WriteLine("Do you wish to add, subtract, multiply or divide?\n");
                 Console.WriteLine("Enter 'Add', 'Subtract', 'Multiply' or 'Divide'.\n");
+                string Choice2 = Console.ReadLine();
+
+                Console.WriteLine("Enter the first number in the equation");
+                int Number1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the second number in the equation");
+                int Number2 = Convert.ToInt32(Console.ReadLine());
+
+                int Result = Calculator(Number1, Number2, Choice2);
+                Console.WriteLine("The result is " + Result + "!");
             }
         }
     }
